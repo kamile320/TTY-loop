1fa234771f58b8dfd53b92843bf0896507b6b361
Krzysztof M <k.marszalowicz@gmail.com>
2026-01-09 19:02:02 +0100

    Update sysctladd.py
    Changes in messages, added auto-chmod 775

--- sysctladd.py
+++ sysctladd.py
@@ -1,21 +1,22 @@
 import os
 maindir = os.getcwd()
-print("TTYloop adding to systemctl program")
+print("TTYloop adding to systemd script")
 
-print("Making autorun.sh ...")
+print("Creating autorun.sh ...")
 try:
     auto = open('autorun.sh', 'w')
     auto.write(f"#!/bin/bash\ncd {maindir}\npython3 ttyloop.py")
     auto.close()
+    os.chmod('autorun.sh', 0o775)
     print("Done.")
 except:
-    print("Can't make autorun.sh\nAre you root?")
+    print("Can't create autorun.sh\nAre you root?")
 
-print('Making TTYloop.service in /etc/systemd/system..')
+print('Creating TTYloop.service in /etc/systemd/system..')
 try:
     sys = open('/etc/systemd/system/TTYloop.service', 'w')
     sys.write(f"[Unit]\nDescription=TTYloop autorun service\n\n[Service]\nExecStart={maindir}/autorun.sh\n\n[Install]\nWantedBy=multi-user.target")
     print("Done!")
-    print("Run post installation commands to enable TTYloop.service to start with system startup:\nsudo chmod -R 777 [TTYloop directory] <== without this permissions will not work\nsudo systemctl enable TTYloop <== enables on system startup\nsudo systemctl start TTYloop <== Optional\nsudo systemctl daemon-reload <== if you're running this command second time\n\nREMEBER about Reading/Executing permissions for others!!!")
+    print("Run post installation commands to enable TTYloop.service to start with system startup:\nsudo chmod -R 777 [TTYloop directory] -> if doesn't have read/execute permissions\nsudo systemctl enable TTYloop -> enables on system startup\nsudo systemctl start TTYloop -> Optional\nsudo systemctl daemon-reload -> if you're running this command second time\n\nREMEMBER about Reading/Executing permissions for others!!!")
 except:
     print("Can't create service file!\nAre you root?")
