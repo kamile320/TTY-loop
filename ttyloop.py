a9bc0ec99d0b46b71ee8f9f1e2f2992f538ccc16
Krzysztof M <k.marszalowicz@gmail.com>
2026-01-09 19:04:09 +0100

    Update ttyloop.py
    added variable to easily manage pause length

--- ttyloop.py
+++ ttyloop.py
@@ -1,15 +1,16 @@
 import subprocess
 import time
+wait = 5
 while True:
-    time.sleep(5)
+    time.sleep(wait)
     subprocess.run(['bash', 'shells/t4.sh'])
-    time.sleep(5)
+    time.sleep(wait)
     subprocess.run(['bash', 'shells/t6.sh'])
-    time.sleep(5)
+    time.sleep(wait)
     subprocess.run(['bash', 'shells/t2.sh'])
-    time.sleep(5)
+    time.sleep(wait)
     subprocess.run(['bash', 'shells/t5.sh'])
-    time.sleep(5)
+    time.sleep(wait)
     subprocess.run(['bash', 'shells/t1.sh'])
-    time.sleep(5)
+    time.sleep(wait)
     subprocess.run(['bash', 'shells/t3.sh'])
